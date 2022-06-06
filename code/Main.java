78d515bccf57d9c86a92504da81c90aca643761e
H.K. Bevin Januka <20535223@student.curtin.edu.au>
2022-06-06 10:14:59 +0530

    Added Read Me to Main Menu

--- code/Main.java
+++ code/Main.java
@@ -19,7 +19,8 @@ public class Main
             System.out.println("Enter Selection from Menu");
             System.out.println("1.Convert String File to Lower Case or Upper Case");
             System.out.println("2.Convert Weight from kg to pounds and vise versa");
-            System.out.println("3.Exit");
+            System.out.println("3.READ ME");
+            System.out.println("4.Exit");
             System.out.println("--------------------------------------------------");
             
             System.out.print("Your Choice: ");
@@ -32,6 +33,19 @@ public class Main
                 case 2:
                     WeightConverter.unitValidation();
                     exit = true;
+                    break;
+                case 3:
+                    System.out.println("-----------------------------------------------------------------");
+                    System.out.println("READ ME:\n");
+                    System.out.println("-> Program is created to allocate two separate input files\n" +
+                            "such that input of strings and weight are independent.\n");
+                    System.out.println("-> In String input file, strings should be added \n" +
+                            "line by line.\n");
+                    System.out.println("-> In Weight input file, weight should be added with 'kg' or 'lbs' \n" +
+                            "as 'kg' for kilograms and 'lbs' for pounds.\n");
+                    System.out.println("-> In Weight input file, weight and unit must be separated \n" +
+                            "a space. For example '50 kg','120 lbs'");
+                    System.out.println("-----------------------------------------------------------------\n");
                     break;
                     
                 default:
